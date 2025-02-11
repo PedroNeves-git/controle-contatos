bb8708d0230cad94957e7a7152e3f9a1fa8d3fdd
PedroNeves-git <sgtpedrocos@gmail.com>
2025-02-10 21:35:48 -0300

    commit inicial

---
+++ src/main/java/br/com/capacitacao/controle_contatos/ControleContatosApplication.java
@@ -0,0 +1,13 @@
+package br.com.capacitacao.controle_contatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleContatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/capacitacao/controle_contatos/ControleContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.capacitacao.controle_contatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
